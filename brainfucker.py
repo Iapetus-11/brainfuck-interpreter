b536ef872e30eafaf513ef52966b455ee6af802b
Milo Weinberg <iapetus011@gmail.com>
2021-01-23 12:37:13 -0500

    more stuff

--- brainfucker.py
+++ brainfucker.py
@@ -1,7 +1,30 @@
-valid = ('>', '<', '+', '-', '.', ',', '[', ']',)
+valid = '><+-.,[]'
 
-bf = input(': ')
-bf = ''.join(c for c in bf if c in valid)
+bf = ''.join(c for c in input(': ') if c in valid)
 
-for c in bf:
-    if c == 
+arr = []
+ptr = 0
+
+def interpreter(bf: str):
+    while i < len(bf):
+        c = bf[i]
+
+        if c == '>':
+            ptr += 1
+        elif c == '<':
+            ptr -= 1
+        elif c == '+':
+            arr[ptr] += 1
+        elif c == '-':
+            arr[ptr] -= 1
+        elif c == '.':
+            print(arr[ptr])
+        elif c == ',':
+            arr[ptr] = int(input(': '))
+        elif c == '[':
+            while arr[ptr]:
+                interpreter(bf[i:])
+        elif c == ']':
+            return
+
+        i += 1
